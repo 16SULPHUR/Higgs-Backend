b7f7d797fdfd6813aeb1ef83ce11ecd0d5a73557
“Ankit <22amtics124@gmail.com>
2025-07-30 19:01:52 +0530

    db connection method changed

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,15 +4,26 @@ dotenv.config();
 
 console.log('Connecting to database...');
 
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false, 
+//   },
+//   connectTimeoutMillis: 5000, 
+//   max: 10,
+//   idleTimeoutMillis: 30000, 
+// });
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, 
-  },
-  connectTimeoutMillis: 5000, 
-  max: 10,
-  idleTimeoutMillis: 30000, 
-});
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  maxLifetimeSeconds: 60
+})
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
